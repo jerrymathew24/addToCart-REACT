210b7f4dd73895f596cdc03de0686d406e52059b
Jerry Mathew <jer2ymatheww@gmail.com>
2025-04-05 23:32:55 +0530

    add to cart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const allBrands = [
+  {id: 1, name: 'Nike'},
+  {id: 2, name: 'Adidas'},    
+  {id: 3, name: 'Puma'},
+  {id: 4, name: 'Reebok'},
+  {id: 5, name: 'Under Armour'},
+  {id: 6, name: 'New Balance'},
+  {id: 7, name: 'Asics'},
+  {id: 8, name: 'Converse'},
+  {id: 9, name: 'Vans'}
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  let [selectedBrand,setSelectedBrand] = useState([])
+
+  const onAddToCartClick = (id) =>{
+    const selectedItem = allBrands.find(item => item.id === id)
+    setSelectedBrand([...selectedItem])
+  }
+
+    return (
+      <>
+        <div>
+          <p>Add Brands to your cart</p>
+          {
+            allBrands.map(brand => 
+            <div>
+              <span>{brand.name}</span>
+              <button onClick={()=>onAddToCartClick(brand.id)}>Add To Cart</button>
+            </div>)
+          }
+          <div>
+            <p> YOUR CART</p>
+            {
+              selectedBrand.map(brand => <span>{brand.name}</span>)
+            }
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  export default App;
